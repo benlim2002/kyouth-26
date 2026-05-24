83d261c478f74335c2cd55d1b756a84574bf978d
Ben Lim Chong Chuen <102507216+benlim2002@users.noreply.github.com>
2026-05-25 05:06:32 +0800

    feat: implementing day 2

--- week3/frontend/src/app.py
+++ week3/frontend/src/app.py
@@ -1,11 +1,18 @@
+import os
+from dotenv import load_dotenv
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 
-app = FastAPI()
+load_dotenv()
 
+app = FastAPI()
 templates = Jinja2Templates(directory="src/templates")
+
+BACKEND_URL = os.getenv("BACKEND_URL", "http://localhost:8001")
 
 
 @app.get("/")
 async def index(request: Request):
-    return templates.TemplateResponse(request, "chat_page.html")
+    return templates.TemplateResponse(
+        request, "chat_page.html", {"backend_url": BACKEND_URL}
+    )
